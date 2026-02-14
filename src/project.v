366756a973f16294911b7f6d47ce6bf44bc684c7
Miche <michelleskater28@gmail.com>
2026-02-13 19:44:48 -0500

    instantiate spi module in top

--- src/project.v
+++ src/project.v
@@ -45,5 +45,19 @@ module tt_um_uwasic_onboarding_Michelle_Chen (
     .out({uio_out, uo_out})
   );
 
+  // Instatiate SPI module
+  spi_peripheral spi_peripheral_inst (
+    .clk(clk),
+    .sclk(ui_in[0]),
+    .rst_n(rst_n),
+    .ncs(ui_in[2]),  
+    .COPI(ui_in[1]), 
+
+    .en_out_uo(en_reg_out_7_0),
+    .en_out_uio(en_reg_out_15_8),
+    .en_pwm_uo(en_reg_pwm_7_0),
+    .en_pwm_uio(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle)
+  );
 
 endmodule
